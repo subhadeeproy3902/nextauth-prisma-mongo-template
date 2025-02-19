d135f31dd6b9093f65665d8aa2c1ab86dd4763fc
subhadeeproy3902 <subha9.5roy350@gmail.com>
2025-02-19 13:15:40 +0530

    feat: enhance authentication flow and improve dashboard layout with user greeting and recent activity

--- src/app/(auth)/layout.tsx
+++ src/app/(auth)/layout.tsx
@@ -1,8 +1,16 @@
-export default function AuthLayout({
+import { getAuthenticatedUser } from "@/utils/authenticatedUser";
+import { redirect } from "next/navigation";
+
+export default async function AuthLayout({
   children,
 }: Readonly<{
   children: React.ReactNode;
 }>) {
+  const dbUser = await getAuthenticatedUser();
+  if (dbUser) {
+    redirect("/dashboard");
+  }
+
   return (
     <div className="relative flex h-screen w-full flex-col items-center justify-center bg-muted">
       {children}
--- src/app/(root)/dashboard/page.tsx
+++ src/app/(root)/dashboard/page.tsx
@@ -1,12 +1,44 @@
-export default function Dashboard() {
+import { CardsStats } from "@/components/activity-goal";
+import RecentActivity from "@/components/recent-activity";
+import { Button } from "@/components/ui/button";
+import {
+  Card,
+  CardContent,
+  CardFooter,
+  CardHeader,
+  CardTitle,
+} from "@/components/ui/card";
+import { getAuthenticatedUser } from "@/utils/authenticatedUser";
+
+export default async function Dashboard() {
+  const dbUser = await getAuthenticatedUser();
+
   return (
     <div className="flex flex-1 flex-col gap-4 p-4 pt-0">
       <div className="grid auto-rows-min gap-4 md:grid-cols-3">
-        <div className="aspect-video rounded-xl bg-muted/50" />
-        <div className="aspect-video rounded-xl bg-muted/50" />
-        <div className="aspect-video rounded-xl bg-muted/50" />
+        <Card>
+          <CardHeader className="flex flex-row items-center justify-between space-y-0 pb-0">
+            <CardTitle className="text-sm font-normal">Welcome!</CardTitle>
+          </CardHeader>
+          <CardContent className="pb-0">
+            <div className="text-2xl font-bold">
+              {(dbUser.name && dbUser.name) || "User"}
+            </div>
+            <p className="text-xs text-primary">{dbUser.email}</p>
+            <p className="text-xs text-muted-foreground mt-2">
+              Welcome to your dashboard. Here you can view your recent activity
+              and update your profile.
+            </p>
+          </CardContent>
+          <CardFooter className="mt-2 w-full">
+            <Button>Edit Profile</Button>
+          </CardFooter>
+        </Card>
+        <CardsStats />
       </div>
-      <div className="min-h-[100vh] flex-1 rounded-xl bg-muted/50 md:min-h-min" />
+      <div className="min-h-[100vh] flex-1 rounded-xl bg-muted/50 md:min-h-min">
+        <RecentActivity />
+      </div>
     </div>
   );
 }
